a401121911e76b27aaa210d05380f9a7a1d312ad
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-10 15:41:49 -0600

    Ejemplo Sintaxis Spread

---
+++ Sintaxis Spread/scritp.js
@@ -0,0 +1,14 @@
+/*function iniciarCarros(car1,car2,car3){ 
+    console.log(car1, car2, car3);
+ } 
+ let idCarros = [100,200,300];
+ iniciarCarros(...idCarros);
+  */
+
+
+ function iniciarCarros(car1,car2,car3){ 
+    console.log(car1, car2, car3);
+ } 
+ let codigoCarros = 'abc'
+ iniciarCarros(...codigoCarros);
+  
