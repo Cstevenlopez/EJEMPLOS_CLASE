d035f99e3a73943e2dab0a4f914c744aee5b6be3
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-10 14:58:21 -0600

    Ejemplo parametro Rest

---
+++ Parámetros Rest/scritp.js
@@ -0,0 +1,13 @@
+function enviarCarros(...idCarros)
+{ 
+   idCarros.forEach(id => console.log(id));
+} 
+enviarCarros(100,200,300);
+
+
+/*unction enviarCarros(dia,...idCarros)
+{ 
+   idCarros.forEach(id => console.log(id));
+} 
+enviarCarros('Miercoles',100,200,300);
+*/
