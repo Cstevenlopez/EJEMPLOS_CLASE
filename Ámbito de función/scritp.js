90aaa1b9d258abb64b544c8fc08f34c7b7d80ec5
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-10 16:16:55 -0600

    Ejemplo de Ambito de funcion

---
+++ Ámbito de función/scritp.js
@@ -0,0 +1,16 @@
+/*function iniciarCarro(idCarro)
+{ 
+   let mensaje = 'Iniciando...';
+} 
+iniciarCarro(123);
+console.log(mensaje); 
+*/
+function iniciarCarro(idCarro){ 
+   let mensaje = 'Iniciando...';
+   let iniciarFn = function iniciarCarro(){
+       let mensaje = 'Reemplazando'; 
+      };
+      iniciarFn();
+      console.log(mensaje); 
+   }
+   iniciarCarro(123);
