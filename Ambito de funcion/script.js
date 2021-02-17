55e73b17fbe65ebf75dcf736961eb9ad7adfeac7
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-17 14:08:31 -0600

    Ambito de funcion

---
+++ Ambito de funcion/script.js
@@ -0,0 +1,18 @@
+function iniciarCarro(idCarro)
+{ 
+   let mensaje = 'Iniciando...';
+} 
+iniciarCarro(123);
+console.log(mensaje); 
+//==========================================
+
+
+function iniciarCarro(idCarro){ 
+   let mensaje = 'Iniciando...';
+   let iniciarFn = function iniciarCarro(){
+       let mensaje = 'Reemplazando'; 
+   };
+   iniciarFn();
+   console.log(mensaje); // ‘Iniciando’
+} 
+iniciarCarro(123);
