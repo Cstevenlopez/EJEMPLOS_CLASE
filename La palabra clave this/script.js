4aea2809cf75469e3c57cbd10833041b56149f26
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-17 13:36:11 -0600

    palabra clave this

---
+++ La palabra clave this/script.js
@@ -0,0 +1,14 @@
+let fn = function (){ 
+    console.log(this === window);
+};
+
+fn(); // true
+
+let o = {
+    carId: 123,
+    getId: function(){
+	 return this.carId;
+    } 
+};
+
+console.log( o.getId()); 
