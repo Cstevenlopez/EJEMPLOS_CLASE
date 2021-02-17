1d5853053bc05f0ed70a691cf51ebcfbce13b952
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-17 14:08:12 -0600

    Ambito de bloque

---
+++ Ambito de bloque/script.js
@@ -0,0 +1,13 @@
+if (5 == 5){ 
+    let mensaje = 'Igual...';
+ } 
+ console.log(mensaje); 
+ //=================================
+
+
+ let mensaje = 'Fuera...';
+if (5 == 5){ 
+   let mensaje = 'Igual...';
+   console.log(mensaje); 
+}
+   console.log(mensaje); 
