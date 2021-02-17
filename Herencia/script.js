50cb164394f20084ca9e46fa99c995ba2a9d9eca
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-17 13:37:30 -0600

    Herencia

---
+++ Herencia/script.js
@@ -0,0 +1,14 @@
+class Vehiculo {
+    constructor(id){
+      this.tipo = 'carro';
+    }
+    iniciar(){
+      return `Iniciando: ${this.tipo}`;
+    }
+}
+class Carro extends Vehiculo {
+
+}
+
+let carro = new Carro();
+console.log( carro.tipo ); //carro
