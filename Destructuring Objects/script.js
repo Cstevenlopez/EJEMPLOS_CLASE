a537bacca8b4b694b601c41622ef9b587f0928c8
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-17 14:06:49 -0600

    Porbando array

---
+++ Destructuring Objects/script.js
@@ -0,0 +1,14 @@
+let carro = {id:20,estilo: 'convertible'}; 
+let id,estilo;
+{id,estilo} = carro; 
+console.log(id,estilo);
+
+let carro = {id:20,estilo: 'convertible'}; 
+let id,estilo;
+({id,estilo} = carro); 
+console.log(id,estilo); 
+
+
+
+
+
