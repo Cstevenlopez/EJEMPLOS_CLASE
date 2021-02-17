7d09991495fe053ddbda4eaa4aa0d53415b52c0b
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-17 13:33:28 -0600

    metodos

---
+++ Metodos/script.js
@@ -0,0 +1,15 @@
+class Carro {
+    constructor(id){
+      this.id = id;
+    }
+    identificar(){
+      return `Carro Id: ${this.id}`;
+    }
+}
+
+let carro = new Carro(123);
+console.log( carro.id ); // 123
+
+
+
+
